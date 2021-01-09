b0d267ffdb663d5e32184a80b3b6906530de4c35
Silvio Marques <silvio.mms2016@gmail.com>
2021-01-09 20:32:49 -0300

    chore: adjusting jest config

--- jest.config.js
+++ jest.config.js
@@ -2,12 +2,8 @@ module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.{ts,tsx}',
-    '!<rootDir>/src/main/**/*',
-    '!<rootDir>/src/**/index.ts',
-    '!**/*.d.ts'
   ],
   coverageDirectory: 'coverage',
-  setupFilesAfterEnv: ['<rootDir>/src/main/config/jest-setup.ts'],
   testPathIgnorePatterns: [
     '<rootDir>/node_modules/',
     '<rootDir>/src/main/test/cypress'
